844bf4ba0890319b55a8cdf5126a6d574af675f7
Shreya Shrivastava <147529420+ShreyaS-2321@users.noreply.github.com>
2025-02-16 09:52:46 +0530

    Update q1.c

--- Assg5/q1.c
+++ Assg5/q1.c
@@ -2,7 +2,10 @@
 
 int main()
 {
- int x=123;
- int a=x/10;
- int b=   
+ int x,y;
+ printf("enter value of x\n");
+ scanf("%d",&x);
+ y=(x/100)+((x/10)%10)+x%10;
+ printf("sum of digits of x:%d",y);
+ return 0;
 }
